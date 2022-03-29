7f422a497434b4536845dbdf64f5e2e30eb48a9f
muzafar nazir <60464242+Muzafar-Nazir@users.noreply.github.com>
2022-03-29 11:30:25 +0500

    theme colors

---
+++ quran_tabseer/lib/utils/custom_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class CustomColors {
+  static const Color primerycolor = Color(0xff0097A7);
+  static const Color buttoncolor = Color(0xff607D8B);
+  static const Color text_icon_color = Color(0xffFFFFFF);
+  static const Color dividercolor = Color(0xffBDBDBD);
+  static const Color secondary_text_color = Color(0xff757575);
+  static const Color primarytext_color = Color(0xff212121);
+}
